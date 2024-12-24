3ffe82103166d05d12c1d69307ec7656b9710739
Shizyn412 <juetjuifjhhsiehykeh@mail.ru>
2024-12-24 21:33:55 +0500

    класс Client. Осипов

---
+++ Server/Classes/Client.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Server.Classes
+{
+    class Client
+    {
+        public string Token { get; set; }
+        public DateTime DateConnect { get; set; }
+
+        public Client()
+        {
+            Random random = new Random();
+            string Chars = "iuwgeiugkgdgiugowerugh98y24w9u8948h";
+
+            Token = new string(Enumerable.Repeat(Chars, 15).Select(x => x[random.Next(Chars.Length)]).ToArray());
+            DateConnect = DateTime.Now;
+        }
+    }
+}
